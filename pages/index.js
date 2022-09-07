6c2b7af56845b39882501d9c5868e4b5c6b9ff23
ebenezerdon <ebenezerdonu@gmail.com>
2022-09-07 21:49:54 +0100

    fix casing

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import styles from '../styles/home.module.css'
+import styles from '../styles/Home.module.css'
 
 const Home = ({ posts }) => {
   return (
