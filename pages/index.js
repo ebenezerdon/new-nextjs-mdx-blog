74e76acda880c06a14daa2551f94f2549f4f2aa8
ebenezerdon <ebenezerdonu@gmail.com>
2022-09-27 12:51:47 +0100

    add title

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,33 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Head from 'next/head'
 
 const Home = ({ posts }) => {
   return (
-    <div className={styles.articleList}>
-      {posts.map((post, index) => (
-        <Link href={'/blog/' + post.slug} passHref key={index}>
-          <a className={styles.item}>
-            <div className={styles.thumbnail}>
-              <Image src={post.frontmatter.thumbnailUrl} alt="thumbnail" width={500} height={400} objectFit="cover" />
-            </div>
+    <>
+      <Head>
+        <title>My Next.js Blog</title>
+      </Head>
 
-            <div>
-              <h2 className={styles.title}>{post.frontmatter.title}</h2>
-              <p className={styles.description}>{post.frontmatter.description}</p>
-              <p className={styles.date}>{post.frontmatter.date}</p>
-            </div>
-          </a>
-        </Link>
-      ))}
-    </div>
+      <div className={styles.articleList}>
+        {posts.map((post, index) => (
+          <Link href={'/blog/' + post.slug} passHref key={index}>
+            <a className={styles.item}>
+              <div className={styles.thumbnail}>
+                <Image src={post.frontmatter.thumbnailUrl} alt="thumbnail" width={500} height={400} objectFit="cover" />
+              </div>
+
+              <div>
+                <h2 className={styles.title}>{post.frontmatter.title}</h2>
+                <p className={styles.description}>{post.frontmatter.description}</p>
+                <p className={styles.date}>{post.frontmatter.date}</p>
+              </div>
+            </a>
+          </Link>
+        ))}
+      </div>
+    </>
   )
 }
 
